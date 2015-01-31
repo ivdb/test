8456d66e9f5be33c1055f7be32246f986b528c87
ivdb <i.vandebovenkamp@ma-web.nl>
2015-01-31 12:22:38 +0100

    my first commit

---
+++ 3Variabelen_1.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: i.vandebovenkamp
+ * Date: 22-1-2015
+ * Time: 18:45
+ */
+for($i=0;$i<10;$i++){
+    echo $i . "<br />";
+}
+//test GIT
+for($i=1;$i<=10;$i++){
+    $output = $i * 9;
+
+    echo $i . "*" .  9 .  "=" . $output . "<br />";
+}
---
+++ 3variabelen_3.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+</head>
+
+<body>
+<div class="bla">
+    <p id="blabla">
+        hello
+    </p>
+</div>
+<table>
+
+
+    <?php
+
+    $number = 9;
+
+    for($i=1;$i<=10;$i++){
+        $output = $i * $number;
+        echo $i . "*" .  $number .  "=" . $output . "<br />";
+    }
+    ?>
+
+
+</table>
+
+</body>
+</html>
